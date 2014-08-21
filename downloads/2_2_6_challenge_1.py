d67f4befa69fcf8e2fdc6480e512bb49d4534935
qliu <Qing.Liu@du.edu>
2014-08-21 05:25:28 -0600

    Add 2.2.6

---
+++ downloads/2_2_6_challenge_1.py
@@ -0,0 +1,6 @@
+#####################
+# 2.2.6 Challenge 1
+#####################
+
+school_1 = "Elementary"
+school_2 = "Senior"
---
+++ downloads/2_2_6_challenge_1_answer.py
@@ -0,0 +1,22 @@
+#####################
+# 2.2.6 Challenge 1
+#####################
+
+school_1 = "Elementary"
+school_2 = "Senior"
+
+if school_1 == "Elementary":
+    print school_1
+elif school_1 == "Senior":
+    school_1 = "High"
+    print school_1
+else:
+    print "Unknown"
+
+if school_2 == "Elementary":
+    print school_2
+elif school_2 == "Senior":
+    school_2 = "High"
+    print school_2
+else:
+    print "Unknown"
